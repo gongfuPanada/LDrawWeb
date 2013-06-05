4ab9427c9c29e39200911f9587b3ed44742ae1f9
Park Joon-Kyu <segfault87@gmail.com>
2013-06-05 22:50:19 +0900

    irestructuring

---
+++ ldrawlib/ldraw.dart
@@ -0,0 +1,7 @@
+// Copyright 2013 Park "segfault" Joon Kyu <segfault87@gmail.com>
+
+library ldraw;
+
+part 'ldraw/io.dart';
+part 'ldraw/types.dart';
+part 'ldraw/util.dart';
--- server/static/scripts/ldraw.dart
+++ server/static/scripts/ldraw.dart
@@ -1,7 +1,1 @@
-// Copyright 2013 Park "segfault" Joon Kyu <segfault87@gmail.com>
-
-library ldraw;
-
-part 'ldraw/io.dart';
-part 'ldraw/types.dart';
-part 'ldraw/util.dart';
+../../../ldrawlib/ldraw.dart
